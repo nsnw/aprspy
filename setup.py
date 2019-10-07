db053491113ab5a93798732d7f2bf2743f8eb3d6
Andy Smith <andy@nsnw.ca>
2019-10-07 01:38:06 -0600

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,21 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="aprspy",
-    version="0.1",
+    version="0.1.1",
     author="Andy Smith",
     author_email="andy@nsnw.ca",
     description="An APRS parser for Python",
     long_description=long_description,
     url="https://github.com/nsnw/aprspy",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "geopy"
     ],
     python_requires='>=3.7',
 )
