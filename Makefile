1590b5ccd6e77dcd29d16b0020e1b59998c77f47
Andy Smith <andy@nsnw.ca>
2019-11-05 22:15:03 -0700

    dev-requirements.txt updates and build/documentation cleanup.
    * Added rstvalidator and sphinx-rtd-theme to dev-requirements.txt
    * Switched documentation theme
    * Added documentation build steps to the Makefile
    * Removed built documentation from the repository

--- Makefile
+++ Makefile
@@ -1,6 +1,17 @@
 test:
 	pytest
 
+check_readme:
+	python -m rstvalidator README.rst
+
+cleandoc:
+	make -C docs/ clean
+
+htmldoc:
+	make -C docs/ html
+
+doc: cleandoc htmldoc
+
 dist:
 	python setup.py sdist bdist_wheel
 
@@ -10,4 +21,4 @@ clean:
 upload:
 	twine upload dist/*
 
-release: clean dist upload
+release: clean check_readme cleandoc htmldoc cleandoc dist upload
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../..'))
 
 from aprspy import __version__
@@ -32,7 +33,8 @@ release = __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
