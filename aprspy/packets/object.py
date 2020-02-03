934c1f42be2d575fb0b828816f476eebc376e3c3
Andy Smith <andy@nsnw.ca>
2020-02-02 23:15:27 -0700

    Added base for object packets

--- aprspy/packets/object.py
+++ aprspy/packets/object.py
@@ -10,11 +10,14 @@ logger = logging.getLogger(__name__)
 
 class ObjectPacket(GenericPacket):
 
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def __repr__(self):
         if self.source:
             return "<ObjectPacket: {}>".format(self.source)
         else:
             return "<ObjectPacket>"
+
+    def _parse(self) -> bool:
+        return True
